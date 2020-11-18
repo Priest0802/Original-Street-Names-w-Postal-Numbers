29605a076e9c7d8ede9e77b705ef19c1eadb4df6
Priest0802 <31041212+Priest0802@users.noreply.github.com>
2020-11-18 14:06:28 -0500

    v1.5
    Updated to fxmanifest
    Updated to minimap and main main supported - both with county boarder and without boarder

---
+++ FX Server Resources/postalcodes-with border/fxmanifest.lua
@@ -0,0 +1,9 @@
+-- Resource Metadata
+fx_version 'cerulean'
+games { 'gta5' }
+
+author 'Priest0802'
+description ' Map Postal & Mini Map Postal Script '
+version '3.0.0'
+
+client_script 'client.lua'
